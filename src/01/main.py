d3489a3cb9fa34f840b05585f9a3815a66a0c773
Jakob Ramirez <jaekop@Jakobs-MacBook-Air.local>
2025-10-07 16:21:54 -0700

    Third installation, added the fill in the blank statements for more interactivity

--- src/01/main.py
+++ src/01/main.py
@@ -18,6 +18,10 @@ from visualize import show_fit
 #-------- Usage --------
 # python main.py [csv_name] [COL_X] [COL_Y]
 
+#-------- What you need to fix --------
+# Fill in the blanks (____) and remove comments (--- IGNORE ---)
+# Fill in the blanks at lines 33, 60, 73.
+
 # -------- CLI / Defaults --------
 CSV_PATH = "data/raw/"
 CSV_PATH += sys.argv[1] if len(sys.argv) > 1 else "salaries.csv"
@@ -25,7 +29,8 @@ COL_X    = sys.argv[2] if len(sys.argv) > 2 else "Years of Experience"
 COL_Y    = sys.argv[3] if len(sys.argv) > 3 else "Salary"
 
 # -------- Load & peek --------
-df = pd.read_csv(CSV_PATH)
+# Create the dataframe by loading the CSV (what function can you use)
+# df = pd.____(CSV_PATH)
 
 print("\n=== HEAD ===")
 print(df.head().to_string(index=False))
@@ -51,7 +56,9 @@ for col in [COL_X, COL_Y]:
     )
 
 before = len(data)
-data = data.dropna()
+
+# ~~~~~~~~~ drop rows in data with NAs (what function can you use) ~~~~~~~~~
+
 print(f"\nDropped {before - len(data)} rows after cleaning & removing NAs.")
 
 # -------- Regression --------
@@ -61,7 +68,10 @@ y = data[COL_Y].to_numpy()
 if len(x) < 2:
     raise ValueError("Not enough data points to fit a line.")
 
-m, b = np.polyfit(x, y, 1,)
+# ~~~~~~~~~ fit a line to x, y (what function can you use) ~~~~~~~~~
+# ~~~~~~~~~ store slope in m and intercept in b ~~~~~~~~~
+# Hint: m, b = np.____(____, ____, 1)
+
 print(f"\nRegression: {COL_Y} ≈ {m:.4f} * {COL_X} + {b:.4f}")
 
 # -------- Plot --------
